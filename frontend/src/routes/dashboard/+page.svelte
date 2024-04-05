63b440f6f826021e89422d424efe0f9cb0f172b6
Stanislas MEZUREUX <stanislas.mezureux@telecomnancy.eu>
2024-04-05 13:12:56 +0200

    [FIX](front) check is api response is undefined before displaying chart

--- frontend/src/routes/dashboard/+page.svelte
+++ frontend/src/routes/dashboard/+page.svelte
@@ -28,7 +28,7 @@
 		TimeScale
 	);
 
-	let res;
+	let res = undefined;
 	let data;
 	let options;
 
@@ -85,7 +85,19 @@
 <AuthenticatedLayout>
 	<h1 class="text-3xl font-bold">Welcome!</h1>
 	<section>
-		<Line {data} {options} />
+		{#if res}
+			<Line {data} {options} />
+		{:else}
+			<div
+				class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+				role="status"
+			>
+				<span
+					class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+					>Loading...</span
+				>
+			</div>
+		{/if}
 	</section>
 	<button
 		class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
